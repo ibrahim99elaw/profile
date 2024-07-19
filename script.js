01eeac2e5c16cfe3aa6e8f480ff66eb409b2a41b
ibrahim99elaw <ibrahimhanielaw@gmail.com>
2024-07-19 05:18:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+  
+var btnMode =document.querySelector('.btn-mode');
+    btnMode.onclick=function()
+    {
+        document.body.classList.toggle("white-them")
+    };
+
+    console.log(Number.MAX_SAFE_INTEGER);
+    console.log(Number.MIN_SAFE_INTEGER)
+
